f8722fb7f19fdb10392fbd7cd17ac5058822ce61
Julian Löhr <julian.loehr@outlook.com>
2015-07-16 23:53:23 +0200

    Refactored Device
    Removed excessive comments
    Changed variable names
    Removed Driver Interface

--- Xbox360 Controller Bus/Device.h
+++ Xbox360 Controller Bus/Device.h
@@ -14,28 +14,15 @@ Environment:
 
 --*/
 
-#include "public.h"
+#include "Public.h"
 
-//
-// The device context performs the same job as
-// a WDM device extension in the driver frameworks
-//
 typedef struct _DEVICE_CONTEXT
 {
-    ULONG PrivateDeviceData;  // just a placeholder
-
+	ULONG Foo;
 } DEVICE_CONTEXT, *PDEVICE_CONTEXT;
 
-//
-// This macro will generate an inline function called DeviceGetContext
-// which will be used to get a pointer to the device context memory
-// in a type safe manner.
-//
 WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(DEVICE_CONTEXT, DeviceGetContext)
 
-//
-// Function to initialize the device and its callbacks
-//
 NTSTATUS
 Xbox360ControllerBusCreateDevice(
     _Inout_ PWDFDEVICE_INIT DeviceInit
